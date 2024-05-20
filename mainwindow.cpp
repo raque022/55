634abcb71dbb6d5fd8e95947e281ca834e0b8c42
Alex Montero <69737872+Alexmv235@users.noreply.github.com>
2024-05-20 01:48:50 -0600

    dibujar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,103 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMainWindow>
+
+#include <QGraphicsScene>
+#include "QPolygonF"
+#include <QGraphicsView>
+#include <iostream>
+
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+        : QMainWindow(parent)
+
+        , ui(new Ui::MainWindow)
+
 {
+
     ui->setupUi(this);
+    
+
+
+    scene = new QGraphicsScene(this);
+
+
+    ui->graphicsView->setScene(scene);
+
+    connect(ui->dibujarBtn, &QPushButton::clicked, this, &MainWindow::onDibujarClicked);
+
+
 }
 
+
 MainWindow::~MainWindow()
+
 {
+
     delete ui;
+
+}
+
+
+void MainWindow::onDibujarClicked()
+
+{
+
+    int numLados = ui->numSidesSpinBox->value();
+
+    double tammannoLados = ui->sideSizeSpinBox->value();
+
+    QRectF viewRect = ui->graphicsView->viewport()->rect();
+
+
+    // Clear the existing polygon
+
+    ui->graphicsView->scene()->clear();
+
+
+    double radio=(tammannoLados/(2* sin(M_PI/numLados)));
+
+    double radioMax=((viewRect.width()-20) /(devicePixelRatio()/2.54))/2;
+    double factor=radioMax/radio;
+
+    std::cout<<"radio: "<<radio<<"\n";
+
+    QPolygon polygon(numLados);
+
+    for (int i = 0; i < numLados; ++i) {
+
+        double angle = ((i * 2 * M_PI )/numLados);
+        std::cout<<"i:"<<i<<"angulo: "<<angle;
+
+        int x = qIntCast(factor) * radio * cos(angle);
+
+        int y = qIntCast(factor) * radio * sin(angle);
+
+        polygon[i]=(QPoint(x, y));
+
+    }
+    // Scale the polygon to fit in the view
+
+    QRectF boundingRect = polygon.boundingRect();
+
+    double scaleFactor = qMin((viewRect.width()-20) / boundingRect.width(),
+
+                              (viewRect.height()-20)/ boundingRect.height());
+    QTransform transform;
+
+    transform.scale(scaleFactor, scaleFactor); // Set the scale factor
+
+    polygon= transform.map(polygon);
+
+    // Draw the polygon in the view
+
+    scene->clear();
+
+    scene->addPolygon(polygon);
+
+    scene->setSceneRect(polygon.boundingRect());
+
+    ui->labelScala->setText(QString("Escala 1:%1 cm").arg(1/factor));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,51 @@
 #ifndef MAINWINDOW_H
+
 #define MAINWINDOW_H
+
 
 #include <QMainWindow>
 
+#include <QGraphicsScene>
+
+#include <QGraphicsView>
+#include <QLabel>
+
+
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+
+namespace Ui { class MainWindow; }
+
 QT_END_NAMESPACE
 
+
 class MainWindow : public QMainWindow
+
 {
-    Q_OBJECT
+
+Q_OBJECT
+
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
+
+private slots:
+
+    void onDibujarClicked();
+
+
 private:
+
     Ui::MainWindow *ui;
+    QPolygon polygon;
+    QLabel* labelScala;
+    QGraphicsView* graphicsView;
+    QGraphicsView* polygonView;
+    QGraphicsScene *scene;
+
 };
+
 #endif // MAINWINDOW_H
